a3e6f100ab6d608d470192e4d07ebf3c9041bc80
Alka Yadav <yalka834@gmail.com>
2025-08-30 13:17:04 +0000

    Move_Zero_at_end

---
+++ Move_Zero.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Move_Zero{
+      public static void print(int []n ){
+            for(int ele: n){
+                System.out.print(ele+" ");
+            }
+            System.out.println();
+        } 
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the size of array: ");
+        int size=sc.nextInt();
+        int [] n= new int[size];
+         System.out.print("enter the elements: ");
+        for(int i=0;i<size;i++){
+            n[i]=sc.nextInt();
+        } 
+        print(n);
+        for(int i=0;i<n.length-1;i++){
+                for(int j=0;j<n.length-1-i;j++){
+                    if(n[j]==0){ 
+                    int temp=n[j];
+                    n[j]=n[j+1];
+                    n[j+1]=temp;
+                }
+                }
+            }
+            print(n);
+    }
+}
